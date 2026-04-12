a5fb3dea0e41b9ad5c99d88214f8740c34aa5abd
Lucas Ornellas <161985274+LOrnellas131@users.noreply.github.com>
2026-04-12 13:35:24 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/cursos', function () {
+    return view('cursos');
+});
+Route::get('/contato', function () {
+    return view('contato');
+});
